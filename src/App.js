cbb52db463fad3b4d6c023173891a793530076e9
FlimothyCrow <timshogren@gmail.com>
2022-04-19 22:31:51 -0600

    created new react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import drums from "./img/drums.jpg"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={drums} className="App-logo" alt="logo" />
+                <img src={logo} className="App-logo" alt="logo" />
+                <a
+                    className="App-link"
+                    href="https://www.youtube.com/watch?v=aOHi8dJdUbI"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Click for spicy drums
+                </a>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
